6779e7c4dc6c9db8c65d107bfb28d51fa5936ad0
Iryna Hromska <irahromska@gmail.com>
2025-09-08 08:59:50 +0300

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,1 +1,20 @@
-# write serializers here
+from django.db.models import CharField
+from rest_framework import serializers
+
+from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ("id", "name",)
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Actor
+        fields = ("id", "first_name", "last_name", "full_name",)
+
+    def get_full_name(self, obj: Actor) -> str:
